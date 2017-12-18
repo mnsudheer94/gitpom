1eee7bc8c8304ce4a46c589a470ad2410fddc4b9
mnsudheer94 <33679309+mnsudheer94@users.noreply.github.com>
2017-12-18 11:29:46 +0530

    updated tech

--- App.java
+++ App.java
@@ -5,26 +5,4 @@ public String Sample() {
 	return "sample";
 }
 }
-
-package vms;
-
-public class App {
-public String Sample() {
-	return "sample";
-}
-}
-
-
-package vms;
-
-public class App {
-public String Sample() {
-	return "sample";
-}
-}package vms;
-
-public class App {
-public String Sample() {
-	return "sample";
-}
-}
+ tech marshals
